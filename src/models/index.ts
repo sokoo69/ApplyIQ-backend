203fa5ecd27222ca04da256fa76c9e5d8e57b8c3
saykot biswas shawon <shawon.saykot2023@gmail.com>
2026-07-18 16:35:10 +0600

    add models index and remove gitkeep

---
+++ src/models/index.ts
@@ -0,0 +1,13 @@
+export { default as User } from './User.model';
+export { default as Job } from './Job.model';
+export { default as Application } from './Application.model';
+export { default as AIGeneration } from './AIGeneration.model';
+export { default as ChatSession } from './ChatSession.model';
+export { default as MatchFeedback } from './MatchFeedback.model';
+
+export * from './User.model';
+export * from './Job.model';
+export * from './Application.model';
+export * from './AIGeneration.model';
+export * from './ChatSession.model';
+export * from './MatchFeedback.model';
